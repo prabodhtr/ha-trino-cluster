52bb1183543c7d649e6204104896d49b9a8310ab
prabodhtr <prabodhtr@gmail.com>
2024-10-07 11:03:27 +0530

    Update trino images

--- docker/envoy/entrypoint.sh
+++ docker/envoy/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Generating envoy_config.yaml file..."
-cat /etc/envoy/envoy.yaml | envsubst \$CONTROLPLANE_XDS_PORT,\$CONTROLPLANE_CACHE_PORT,\$ENVOY_ADMIN_PORT > /envoy_config.yaml
+cat /etc/envoy/envoy.yaml | envsubst \$CONTROLPLANE_XDS_PORT,\$CONTROLPLANE_HTTP_PORT,\$ENVOY_ADMIN_PORT > /envoy_config.yaml
 
 echo "Starting Envoy..."
 /usr/local/bin/envoy -c /envoy_config.yaml
